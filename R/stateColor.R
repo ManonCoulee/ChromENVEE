7ad20f49dd60c3c75b08b482b6a5057b747143dc
ManonCoulee <manoncoulee@hotmail.com>
2022-05-05 12:02:42 +0200

    create function to associate state to colot

---
+++ R/stateColor.R
@@ -0,0 +1,32 @@
+#' Function to return chromatin state color table
+#'
+#' @param state_name a vector of chromatin state name
+#' @param state_number a vector of chromatin state number
+#' @param color a vector of color to associated to each chromatin state
+#'
+#' @return list of vector with each chromatin state associate to color
+#' @export
+stateColor = function(state_name, state_number, color) {
+
+  ## Test si les 3 variable ont la même taille
+  if(length(state_name) != length(state_number)) {
+    stop("Not same length between each parameters !")
+  }
+  if(length(state_name) != length(color)) {
+    stop("Not same length between each parameters !")
+  }
+  if(length(state_number) != length(color)) {
+    stop("Not same length between each parameters !")
+  }
+
+  ## Creer le tableau
+  colorName = color
+  names(colorName) = state_name
+
+  colorNumber = color
+  names(colorNumber) = state_number
+
+  col = list("state_name" = colorName, "state_number" = colorNumber)
+
+  return(col)
+}
