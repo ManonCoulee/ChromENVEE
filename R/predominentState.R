a42d3c417e09b963255f503cd93c602920107b28
ManonCoulee <manoncoulee@hotmail.com>
2022-06-09 15:22:24 +0200

    create the function

---
+++ R/predominentState.R
@@ -0,0 +1,25 @@
+#' Function with associate at each enhancer the nearly gene
+#'
+#' @param table a table corresponding to the result of geneEnvironment function
+#' @param state a list of chromatin state
+#' @param header a list of column in table to analysed with UMAP
+#' @param neighbor a numeric value
+#' @param metric a chracter value
+#' @param dist a numeric value
+#'
+#' @import umpar
+#'
+#' @return the table with 2-dimensional value and predominent chromatin state
+#' @export
+predominentState = function(table, state, header, neighbors = 32, metric = "euclidean", dist = 0.5) {
+
+  result = umap(table[,header], n_neighbors = neighbors, metric = metric, min_dist = dist)
+
+  result$state = sapply(rownames(result), function(gene) {
+  	pos = which(table[gene,state] == max(table[gene,state]))
+  	x = colnames(table[,state])[pos[1]]
+  	return(x)
+  })
+
+  return(result)
+}
