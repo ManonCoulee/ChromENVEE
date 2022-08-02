310515cb52cfa23e9a0732a3090b5224bff34a22
ManonCoulee <manoncoulee@hotmail.com>
2022-08-02 16:55:01 +0200

    add documentation to geneExpression data

---
+++ R/geneExpression.R
@@ -0,0 +1,14 @@
+#' BED file contains name, position and expression of genes
+#'
+#' @format A data frame with 20639 rows and 7 variables:
+#' \describe{
+#'  \item{gene_ENS}{gene name in ENSEMBL nomenclature}
+#'  \item{chr}{chromosome number}
+#'  \item{start}{start position, in bp}
+#'  \item{end}{end position, in bp}
+#'  \item{strand}{strand}
+#'  \item{score}{score of enrichent}
+#'  \item{gene_expression}{mean of gene expression level, in CPM}
+#'}
+#'
+"geneExpression"
