5eb9099d3d8ea1acf326f7ba21a50c713bd6367a
ManonCoulee <manoncoulee@hotmail.com>
2022-09-08 16:32:50 +0200

    update function test

--- tests/testthat/test_errorFormat.R
+++ tests/testthat/test_errorFormat.R
@@ -4,34 +4,34 @@ library(testthat)
 test_that("Test the conformity of initial data", {
   data_table = c("table","expresion")
 
-  state_name = c("TSS","TSSFlnk","TSSFlnkD")
-  state_number = c("U1","U2")
-  color_value = c("#B71C1C","#E65100","#E65100","#43A047")
+  stateName = c("TSS","TSSFlnk","TSSFlnkD")
+  stateNumber = c("U1","U2")
+  color = c("#B71C1C","#E65100","#E65100","#43A047")
 
   data_frame = data.frame(c(1:10),c(1:10))
 
   ## Test for getStateColor
-  expect_error(getStateColor(state_name,state_number,color_value),
+  expect_error(getStateColor(stateName,stateNumber,color),
     regexp = "Not same length between each parameters !")
 
   ## Test for getInformation
-  expect_error(getInformation(data_table = data_table),
-    regexp = "'data_table' must be a GRanges object or a list of GRanges object")
-  expect_error(getInformation(data_table = data_frame),
-    regexp = "'data_table' must be a GRanges object or a list of GRanges object")
+  expect_error(getInformation(dataTable = data_table),
+    regexp = "'dataTable' must be a GRanges object or a list of GRanges object")
+  expect_error(getInformation(dataTable = data_frame),
+    regexp = "'dataTable' must be a GRanges object or a list of GRanges object")
 
   ## Test for plotChromatinState
-  expect_error(plotChromatinState(genomic_region = data_table),
-    regexp = "'genomic_region' must be a data frame or a list of data frame")
+  expect_error(plotChromatinState(tableChromatinState = data_table),
+    regexp = "'tableChromatinState' must be a data frame or a list of data frame")
 
   ## Test for enhancerAnnotation
-  expect_error(enhancerAnnotation(enhancer_table = data_frame),
-    regexp = "'enhancer_table' is not a GRanges table")
+  expect_error(enhancerAnnotation(enhancerTable = data_frame),
+    regexp = "'enhancerTable' is not a GRanges table")
 
   ## Test for plotEnhancerExpression
-  expect_error(plotEnhancerExpression(distance = "a",data_table = data_frame),
-    regexp = "'distance' must be a numeric object")
-  expect_error(plotEnhancerExpression(scale = "exp" ,data_table = data_frame),
+  expect_error(plotEnhancerExpression(scale = "exp", dataTable = data_frame),
     regexp = "'scale' must be 'none', 'log10','log2' value. Default is 'none'")
 
+  expect_error(plotEnhancerExpression(distance = "0"),
+    regexp = "'distance' must be a numeric object")
 })
--- tests/testthat/test_getClassData.R
+++ tests/testthat/test_getClassData.R
@@ -2,12 +2,12 @@
 library(testthat)
 
 test_that("Test the conformity of initial data", {
-  data_table = c("table","expresion")
+  dataTable = c("table","expresion")
 
-  expect_error(getInformation(data_table),
-    regexp = "'data_table' must be a GRanges object or a list of GRanges object")
+  expect_error(getInformation(dataTable),
+    regexp = "'dataTable' must be a GRanges object or a list of GRanges object")
 
-  expect_error(plotChromatinState(genomic_region = data_table),
-    regexp = "'genomic_region' must be a data frame or a list of data frame")
+  expect_error(plotChromatinState(tableChromatinState = dataTable),
+    regexp = "'tableChromatinState' must be a data frame or a list of data frame")
 
 })
